00b8da6e0a95368179d313c79a912f81d5456818
Léo Candido <leocansil@gmail.com>
2016-08-25 13:39:25 -0300

    atualizações do curso de javascript

---
+++ js/resumeBuilder.js
@@ -0,0 +1,16 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+// $("#main").append("Leo Candido Silva");
+
+// var awesomeThoughts = 'I am Leo and I am AWESOME!';
+
+// console.log(awesomeThoughts);
+
+var email = "cameron@udacity.com";
+var newEmail = email.replace("udacity", "gmail");
+console.log(email);
+console.log(newEmail);
+var awesomeThoughts = "My name is Leo and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace('AWESOME','FUN');
+$('#main').append(funThoughts);
